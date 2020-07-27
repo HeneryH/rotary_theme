906c16a83ee47eac6bd232034b81667a681fa401
Joe Flynn <jjflynn22@gmail.com>
2020-07-27 00:17:57 +0000

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Behaviors for the Rotary_Theme theme.
+ */
+
+(function ($, _, Drupal, drupalSettings) {
+  'use strict';
+
+  Drupal.behaviors.Rotary_Theme = {
+    attach: function (context) {
+
+      // Rotary_Theme JavaScript behaviors goes here.
+    }
+  };
+
+})(window.jQuery, window._, window.Drupal, window.drupalSettings);
